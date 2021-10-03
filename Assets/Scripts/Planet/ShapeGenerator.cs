df7468de654cea5dc1a5c3526371edb3bf64e6ae
Jacinto Mba Cantero <jacmba@gmail.com>
2021-10-03 02:10:10 +0100

    Layered noise

--- Assets/Scripts/Planet/ShapeGenerator.cs
+++ Assets/Scripts/Planet/ShapeGenerator.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class ShapeGenerator
 {
   ShapeSettings settings;
+  NoiseFilter[] noiseFilters;
 
   public ShapeGenerator(ShapeSettings settings)
   {
     this.settings = settings;
+    noiseFilters = new NoiseFilter[settings.noiseLayers.Length];
+    for (int i = 0; i < noiseFilters.Length; i++)
+    {
+      noiseFilters[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
+    }
   }
 
   public Vector3 calculatePointOnPlanet(Vector3 pointOnUnitSphere)
   {
-    return pointOnUnitSphere * settings.planetRadius;
+    float firstLayerValue = 0;
+    float elevation = 0f;
+
+    if (noiseFilters.Length > 0)
+    {
+      firstLayerValue = noiseFilters[0].evaluate(pointOnUnitSphere);
+      if (settings.noiseLayers[0].enabled)
+      {
+        elevation = firstLayerValue;
+      }
+    }
+
+    for (int i = 0; i < noiseFilters.Length; i++)
+    {
+      if (settings.noiseLayers[i].enabled)
+      {
+        float mask = (settings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1f;
+        elevation += noiseFilters[i].evaluate(pointOnUnitSphere) * mask;
+      }
+    }
+    return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
   }
 }
