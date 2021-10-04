ca1d67fce0355a5c2068aca5f800789d42751bab
Jacinto Mba Cantero <jacmba@gmail.com>
2021-10-04 22:05:50 +0100

    Added red planet

--- Assets/Scripts/Planet/ColorSettings.cs
+++ Assets/Scripts/Planet/ColorSettings.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu()]
+[System.Serializable]
 public class ColorSettings : ScriptableObject
 {
   public Gradient planetGradient;
--- Assets/Scripts/Planet/ShapeSettings.cs
+++ Assets/Scripts/Planet/ShapeSettings.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu()]
+[System.Serializable]
 public class ShapeSettings : ScriptableObject
 {
   public float planetRadius = 1f;
