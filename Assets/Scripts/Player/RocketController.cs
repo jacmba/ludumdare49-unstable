a2a17b6a98c987747f7343d3983f83911f14e440
Jacinto Mba Cantero <jacmba@gmail.com>
2021-10-07 01:21:39 +0100

    WIP: Ship movement

--- Assets/Scripts/Player/RocketController.cs
+++ Assets/Scripts/Player/RocketController.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour
 {
+  private Rigidbody body;
   private bool canExit;
+  private float thrust;
+  private GravityBody gravityBody;
+  [SerializeField] private float thrustRate = 100f;
+  [SerializeField] private float rotSpeed = 90f;
+  private Vector3 rotDir;
+
+  void Awake()
+  {
+    gravityBody = GetComponent<GravityBody>();
+  }
+
   void OnEnable()
   {
+    body = GetComponent<Rigidbody>();
     canExit = false;
+    gravityBody.enabled = false;
+  }
+
+  void OnDisable()
+  {
+    gravityBody.enabled = true;
   }
 
   // Update is called once per frame
@@ -22,5 +41,21 @@ public class RocketController : MonoBehaviour
     {
       EventBus.exitRocket();
     }
+
+    thrust = Input.GetAxis("Fire2");
+    if (thrust < 0)
+    {
+      thrust = 0;
+    }
+
+    rotDir = (Input.GetAxis("Horizontal") * Vector3.back + Input.GetAxis("Vertical") * Vector3.right).normalized;
+  }
+
+  void FixedUpdate()
+  {
+    body.AddForce(transform.up * thrust * thrustRate * Time.deltaTime);
+
+    transform.Rotate(rotDir, Space.Self);
+    body.rotation = transform.rotation;
   }
 }
